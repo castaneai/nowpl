5bd159ef7e362297b95d61919b7dc5632e85ce04
castaneai <castaneai@castaneai.net>
2016-07-24 22:22:20 +0900

    hello, nowpl

---
+++ README.md
@@ -0,0 +1,8 @@
+nowpl
+=========
+
+```sh
+$ git clone http://github.com/castaneai/nowple
+$ cd nowple
+$ npm install && npm start
+```
---
+++ src/main.js
@@ -0,0 +1,15 @@
+import electron from 'electron'
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+let mainWindow
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({width: 400, height: 400})
+  mainWindow.loadURL(`file://${__dirname}/renderer/index.html`)
+  mainWindow.on('closed', () => mainWindow = null)
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
